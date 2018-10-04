94e7068abed89b70f5c8f4fcee9c788e29be48b1
clovis <aspnet2017p4a@yandex.com>
2018-10-04 20:30:14 -0300

    2018-10-04 parte 1

---
+++ WebApplication4/Data/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Data
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ WebApplication4/Pages/Usuarios/Cadastrar.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication4.Data;
+
+namespace WebApplication4.Pages.Usuarios
+{
+    public class CadastrarModel : PageModel
+    {
+        [BindProperty]
+        public Usuario usuario { get; set; }
+        public void OnGet(string nome, string senha)
+        {
+            if (usuario == null)
+            {
+                usuario = new Usuario();
+            }
+            usuario.Nome = nome;
+            usuario.Senha = senha;
+        }
+    }
+}
